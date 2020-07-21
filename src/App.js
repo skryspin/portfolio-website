95ed100ed15ec212c49fb04ca6c2d06ec829a6dc
skryspin <None>
2020-07-21 11:54:38 -0400

    Added React Router and a page does not exist page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path = "/">
+          <Home />
+        </Route>
+        <Route path = "/home">
+          <Home />
+        </Route>
+        <Route path="*">
+          <InvalidPath />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          
+          This is my web development playground.
+          Say 'hi' to me in the following places:
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/skryspin/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Seb Kryspin on LinkedIn
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+function InvalidPath() {
+  return (
+    <div>
+      <p>This page doesn't exist. You can either stay here forever, or <Link to="/"> return to civilization.</Link> </p>
+      <blockquote><h1>The Road Not Taken</h1>
+      <p>
+      Two roads diverged in a yellow wood,<br />
+      And sorry I could not travel both<br />
+      And be one traveler, long I stood<br />
+      And looked down one as far as I could<br />
+      To where it bent in the undergrowth;<br />
+      <br />
+      Then took the other, just as fair,<br />
+      And having perhaps the better claim,<br />
+      Because it was grassy and wanted wear;<br />
+      Though as for that the passing there<br />
+      Had worn them really about the same,<br />
+      <br />
+      And both that morning equally lay<br />
+      In leaves, no step had trodden black.<br />
+      Oh, I kept the first for another day!<br />
+      Yet knowing how way leads on to way,<br />
+      I doubted if I should ever come back.<br />
+      <br />
+      I shall be telling this with a sigh<br />
+      Somewhere ages and ages hence:<br />
+      Two roads diverged in a wood, and I—<br />
+      I took the one less traveled by,<br />
+      And that has made all the difference.
+      <br />
+    </p>
+  <footer>
+  — <cite>Robert Frost</cite>
+  </footer>
+  </blockquote>
+  </div>
+  );
+}
