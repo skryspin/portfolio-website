1ea56eb312d94dc06951e69aa576bb5ee35e1f42
skryspin <None>
2020-07-27 16:34:20 -0400

    Added notes on State and data flow

--- src/Practice.js
+++ src/Practice.js
@@ -7,9 +7,63 @@ import './App.css';
 //NOTE: Always start component names with a capital letter
 //NOTE: Props are read-only: never modify props
 //NOTE: Use STATE to modify stuff
+//NOTE: There are three things you should know about setState()
+// 1) Do Not Modify State directly
+//
+// Wrong
+// this.state.comment = 'Hello';
+//
+//correct
+//this.setState({comment: 'Hello'});
+//
+// The only place where you can assign this.state is the constructor
+//
+// ---- States may be updated Asynchronously ---
+//
+// WRONG
+// this.setState({
+//    counter: this.state.counter + this.props.increment,
+// })/
+//
+// CORRECT
+// this.setState((state, props) => ({
+//      counter: state.counter + props.increment
+//}))/
+//
+// ---- State updates are Merged ----
+// When you call setState(), React merges the object you provide into the current state
+//
+//For example, your state may contain several independent variables:
+//  constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     posts: [],
+  //     comments: []
+  //   };
+  // }
+//Then you can update them independently with separate setState() calls:
+//  componentDidMount() {
+  //   fetchPosts().then(response => {
+  //     this.setState({
+  //       posts: response.posts
+  //     });
+  //   });
+  //
+  //   fetchComments().then(response => {
+  //     this.setState({
+  //       comments: response.comments
+  //     });
+  //   });
+  // }
+  // --- The Data Flows Down---
+  // Any state is always owned by some specific component, and any data derived from that state can only affect components "below" them in the tree
 function Practice(props) {
   return (
-    <Clock />
+    <div>
+      <Clock />
+      <Clock />
+      <Clock />
+    </div>
   )
 }
 
@@ -25,12 +79,13 @@ function Practice(props) {
 
 //The Clock, but as a class so we can use state
 class Clock extends React.Component {
+  //#1 React calls the constructor
   constructor(props) {
     super(props);
     this.state = {date: new Date()};
   }
 
-  //Runs after the component output has been rendered to the DOM
+  //#3 Runs after the component output has been rendered to the DOM
   componentDidMount() {
     this.timerID = setInterval(
       () => this.tick(),
@@ -38,16 +93,19 @@ class Clock extends React.Component {
     );
   }
 
+  //#5 if the Clock component is removed from the DOM, this stops the timer
   componentWillUnmount() {
     clearInterval(this.timerID);
   }
 
+  //#4 Every second, tick() is run
   tick() {
     this.setState({
       date: new Date()
     });
   }
 
+  //#2 React calls render
   render() {
     return (
       <div>
